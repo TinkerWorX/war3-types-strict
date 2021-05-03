684743a435337c9863f0626ae6631e439547b9c9
Nikolaj Mariager <nmariager@blizzard.com>
2021-05-03 21:38:13 +0200

    Added single import bundle. Fixed name of polyfill.

---
+++ 1.32.10.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./compat.d.ts" />
+/// <reference path="./common.j.d.ts" />
+/// <reference path="./common.ai.d.ts" />
+/// <reference path="./blizzard.j.d.ts" />
+/// <reference path="./polyfill.d.ts" />
