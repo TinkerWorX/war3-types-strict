047288e526bbd1dd53b031e119b87d3d30333e0a
Nikolaj Mariager <nmariager@blizzard.com>
2022-08-17 23:02:52 +0200

    Reworked to work across multiple versions. Split all definitions into separate files, make it easier to maintain.

--- 1.32.10.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="./compat.d.ts" />
-/// <reference path="./common.j.d.ts" />
-/// <reference path="./common.ai.d.ts" />
-/// <reference path="./blizzard.j.d.ts" />
-/// <reference path="./polyfill.d.ts" />
