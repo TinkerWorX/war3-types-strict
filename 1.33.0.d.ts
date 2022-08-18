173d7475a2d62dfb05eee952ab944b5bc55acbb3
Nikolaj Mariager <nmariager@blizzard.com>
2022-08-18 11:06:50 +0200

    Added definitions for 1.33.0. Add 1.33.0 to the build script. Bumped version.

---
+++ 1.33.0.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./compat.d.ts" />
+/// <reference path="./1.33.0/common.j.d.ts" />
+/// <reference path="./1.33.0/common.ai.d.ts" />
+/// <reference path="./1.33.0/blizzard.j.d.ts" />
+/// <reference path="./polyfill.d.ts" />
